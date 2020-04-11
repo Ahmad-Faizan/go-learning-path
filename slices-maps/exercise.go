94057415a224e535e0ef530968ae4b45d26a3695
Faizan Ahmad <faizan.nasro@gmail.com>
2020-04-11 10:48:36 +0530

    Added exercise to flatten a matrix to a slice

--- slices-maps/exercise.go
+++ slices-maps/exercise.go
@@ -5,6 +5,15 @@ import "fmt"
 func main() {
 	result := uniqueInts([]int{9, 19, 5, 4, 3, 4, 4, 6, 3, 8, 9, 5, 2})
 	fmt.Println(result)
+
+	res := flattenMatrix([][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7},
+		{8, 9, 10, 11, 12, 13},
+		{14, 15, 16, 17, 18},
+		{19, 20}},
+	)
+	fmt.Println(res)
 }
 
 func uniqueInts(duplicates []int) (unique []int) {
@@ -18,4 +27,13 @@ func uniqueInts(duplicates []int) (unique []int) {
 		}
 	}
 	return unique
+}
+
+func flattenMatrix(matrix [][]int) (slice []int) {
+	for _, internalSlice := range matrix {
+		for _, x := range internalSlice {
+			slice = append(slice, x)
+		}
+	}
+	return slice
 }
