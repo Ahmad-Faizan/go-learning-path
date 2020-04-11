f74f611306bac474eec4f628e938f533221df7d6
Faizan Ahmad <faizan.nasro@gmail.com>
2020-04-11 10:50:11 +0530

    Added exercise to create a matrix from a slice

--- slices-maps/exercise.go
+++ slices-maps/exercise.go
@@ -14,6 +14,9 @@ func main() {
 		{19, 20}},
 	)
 	fmt.Println(res)
+
+	resultMatrix := make2D([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}, 6)
+	fmt.Println(resultMatrix)
 }
 
 func uniqueInts(duplicates []int) (unique []int) {
@@ -36,4 +39,25 @@ func flattenMatrix(matrix [][]int) (slice []int) {
 		}
 	}
 	return slice
+}
+
+func make2D(numbers []int, columns int) [][]int {
+	getRows := func(totalSize int, columns int) int {
+		if totalSize%columns != 0 {
+			return (1 + totalSize/columns)
+		}
+		return totalSize / columns
+	}
+
+	matrix := make([][]int, getRows(len(numbers), columns))
+
+	for i, x := range numbers {
+		row := i / columns
+		col := i % columns
+		if matrix[row] == nil {
+			matrix[row] = make([]int, columns)
+		}
+		matrix[row][col] = x
+	}
+	return matrix
 }
